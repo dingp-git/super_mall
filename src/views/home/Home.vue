7700b2688610335322bba4c0ab8e5ce3e5c666fb
dingp-git <githubdingp@126.com>
2021-07-01 16:55:15 +0800

    'home-navbar,recommend'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,28 +1,38 @@
 <template>
-  <!-- <h2>主页</h2> -->
-  <nav-bar class="home-nav">
-    <div slot="center">购物街</div>
-  </nav-bar>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
+  </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from './childComps/HomeSwiper';
+import RecommendView from './childComps/RecommendView';
+
 import { getHomeMultidata } from "network/home";
+
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper,
+    RecommendView
   },
   data() {
     return {
-      result: null,
+      banner: [],
+      recommend: [],
     }
   },
   created() {
     getHomeMultidata().then(res => {
-      this.result = res;
-      console.log(res)
+      this.banner = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
     })
   }
 };
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,29 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banner" :key="item">
+      <a :href="item.link">
+        <img :src="item.image" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+
+export default {
+  name: "HomeSwiper",
+  props: {
+    banner: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+};
+</script>
